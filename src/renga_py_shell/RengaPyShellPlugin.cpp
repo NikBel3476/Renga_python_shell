032804682b9ef2a5fb303a6975acd19fb00a34f1
NikBel3476 <nikita.belkin3476@gmail.com>
2023-08-31 00:03:40 +0400

    chore: update names

---
+++ src/renga_py_shell/RengaPyShellPlugin.cpp
@@ -0,0 +1,88 @@
+#include "RengaPyShellPlugin.hpp"
+#include "Renga/CreateApplication.hpp"
+
+#include "shellwindow.h"
+#include <QApplication>
+#include <QString>
+
+class py_open_form :public Renga::ActionEventHandler {
+public:
+    py_open_form(Renga::IActionPtr action) : Renga::ActionEventHandler(action) {}
+
+    void OnTriggered() override
+    {
+        int argc = 1;
+        char* args[] = { (char*)"RengaPythonShell" };
+        QApplication app(argc, args);
+
+        ShellWindow window;
+        window.setMinimumSize(800, 600);
+
+        window.show();
+        app.exec();
+    }
+
+    void OnToggled(bool checked) override {}
+};
+
+RengaPyShellPlugin::RengaPyShellPlugin() {
+    ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+}
+
+RengaPyShellPlugin::~RengaPyShellPlugin() {
+    ::CoUninitialize();
+}
+
+void RengaPyShellPlugin::addHandler(Renga::ActionEventHandler* pHandler)
+{
+    m_handlerContainer.emplace_back(HandlerPtr(pHandler));
+}
+
+void RengaPyShellPlugin::EditAction(std::wstring pluginDir, Renga::IActionPtr action, std::wstring image_name, const char* tool_descr)
+{
+    auto renga_app = Renga::CreateApplication();
+    action->ToolTip = tool_descr;
+    Renga::IUIPtr renga_ui = renga_app->GetUI();
+    Renga::IImagePtr icon = renga_ui->CreateImage();
+    std::wstring plugin_dir(pluginDir);
+    icon->LoadFromFile((plugin_dir + image_name).c_str());
+    action->PutIcon(icon);
+}
+
+bool RengaPyShellPlugin::initialize(const wchar_t* pluginPath)
+{
+    auto rengaApplication = Renga::CreateApplication();
+
+    if (!rengaApplication)
+        return false;
+
+        //Загрузка библиотеки QT6 для плагина
+//        QString cur_dir = QString::fromStdWString(pluginPath);
+//        QApplication::addLibraryPath(cur_dir);
+
+    Renga::IUIPtr renga_ui = rengaApplication->GetUI();
+    Renga::IUIPanelExtensionPtr panel = renga_ui->CreateUIPanelExtension();
+
+    Renga::IDropDownButtonPtr down_button = renga_ui->CreateDropDownButton();
+    Renga::IImagePtr icon = renga_ui->CreateImage();
+    std::wstring plugin_dir(pluginPath);
+    icon->LoadFromFile((plugin_dir + L"\\repa_logo.png").c_str());
+    down_button->PutIcon(icon);
+    down_button->ToolTip = "Запустить Renga Python Shell";
+
+    //Создаем кнопки для Py-логики
+    //Исполнение из QT формы
+    Renga::IActionPtr button_py_execute = renga_ui->CreateAction();
+    EditAction(pluginPath, button_py_execute, L"\\py_logo.png", "Запустить основное окно");
+    button_py_execute->DisplayName = "Run Python shell";
+    this->addHandler(new py_open_form(button_py_execute));
+    down_button->AddAction(button_py_execute);
+
+    panel->AddDropDownButton(down_button);
+    renga_ui->AddExtensionToPrimaryPanel(panel);
+
+    return true;
+}
+void RengaPyShellPlugin::stop() {
+    this->m_handlerContainer.clear();
+}
--- src/renga_py_shell/RengaPyShellPlugin.hpp
+++ src/renga_py_shell/RengaPyShellPlugin.hpp
@@ -3,6 +3,8 @@
 
 #import "RengaCOMAPI.tlb"
 
+#include <QObject>
+
 #include "Renga/CreateApplication.hpp"
 #include "Renga/IPlugin.h"
 #include "Renga/ActionEventHandler.hpp"
@@ -11,18 +13,25 @@
 #include "memory"
 #include "list"
 
-class Renga_loader : public plugins::IPlugin
+class RengaPyShellPlugin : public QObject, public plugins::IPlugin
 {
+Q_OBJECT
+
 public:
-    bool initialize(const wchar_t* pluginPath);
-    void stop();
+    RengaPyShellPlugin();
+    ~RengaPyShellPlugin();
+
+    bool initialize(const wchar_t* pluginPath) override;
+    void stop() override;
+
 private:
     void EditAction(std::wstring pluginDir, Renga::IActionPtr action, std::wstring image_name, const char* tool_descr);
     void addHandler(Renga::ActionEventHandler* pHandler);
+    bool PyInitialize() { return true; } // for python
+
     typedef std::unique_ptr<Renga::ActionEventHandler> HandlerPtr;
+
     std::list<HandlerPtr> m_handlerContainer;
-    //for python
-    bool PyInitialize() { return true; }
 };
-EXPORT_PLUGIN(Renga_loader);
+EXPORT_PLUGIN(RengaPyShellPlugin);
 #endif // RENGA_LOADER_HPP
